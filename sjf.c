f6a05687a18bea82f5014f0e1362e6a56d91e760
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-06-01 09:58:47 +0530

    refactor: process name assignment to use integers instead of strings

--- sjf.c
+++ sjf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 struct process{
-    char name[10];
+    int name;
     int arrivalTime;
     int burstTime;
     int completionTime;
@@ -15,14 +15,15 @@ void main(){
     printf("Enter the number of processes: ");
     scanf("%d", &n);
     for(int i = 0; i < n; i++){
-        printf("Enter process details (name, arrival time, burst time):\n");
+        printf("Enter process details (arrival time, burst time):\n");
         printf("P%d: ", i + 1);
-        scanf("%s %d %d", p[i].name, &p[i].arrivalTime, &p[i].burstTime);
+        scanf("%d %d", &p[i].arrivalTime, &p[i].burstTime);
         p[i].status = false;
+        p[i].name = i + 1;
     }
     for(int i = 0; i < n - 1; i++){
         for(int j = 0; j < n - 1 - i; j++){
-            if(p[j].burstTime > p[j + 1].burstTime){
+            if((p[j].burstTime > p[j + 1].burstTime) || (p[j].burstTime == p[j + 1].burstTime && p[j].arrivalTime > p[j + i].arrivalTime)){
                 temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
@@ -39,7 +40,7 @@ void main(){
             p[i].completionTime = time;
             p[i].turnaroundTime = p[i].completionTime - p[i].arrivalTime;
             p[i].waitingTime = p[i].turnaroundTime - p[i].burstTime;
-            printf("%s\t%d\t%d\t%d\t%d\t%d\n", p[i].name, p[i].arrivalTime, p[i].burstTime, p[i].completionTime, p[i].turnaroundTime, p[i].waitingTime);
+            printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].name, p[i].arrivalTime, p[i].burstTime, p[i].completionTime, p[i].turnaroundTime, p[i].waitingTime);
             wait = wait + p[i].waitingTime;
             turnaround = turnaround + p[i].turnaroundTime;
         }
