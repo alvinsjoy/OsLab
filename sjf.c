d894f3a0ec80c0b2bf74acbd704a6897fa888f82
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-06-02 15:53:48 +0530

    refactor: optimize SJF scheduling algorithm

--- sjf.c
+++ sjf.c
@@ -7,7 +7,7 @@ struct process{
     int completionTime;
     int waitingTime;
     int turnaroundTime;
-    bool status;
+    bool executed;
 }p[20], temp;
 
 void main(){
@@ -18,33 +18,45 @@ void main(){
         printf("Enter process details (arrival time, burst time):\n");
         printf("P%d: ", i + 1);
         scanf("%d %d", &p[i].arrivalTime, &p[i].burstTime);
-        p[i].status = false;
+        p[i].executed = false;
         p[i].name = i + 1;
     }
     for(int i = 0; i < n - 1; i++){
         for(int j = 0; j < n - 1 - i; j++){
-            if((p[j].burstTime > p[j + 1].burstTime) || (p[j].burstTime == p[j + 1].burstTime && p[j].arrivalTime > p[j + i].arrivalTime)){
+            if((p[j].arrivalTime > p[j + 1].arrivalTime) || (p[j].arrivalTime == p[j + 1].arrivalTime && p[j].burstTime > p[j + 1].burstTime)) {
                 temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
             }
         }
     }
-    int time = 0, i = 0;
+    int time = 0, i = 0, executedCount = 0;
     float wait = 0.0, turnaround = 0.0;
     printf("P\tAT\tBT\tCT\tTAT\tWT\n");
-    while(p[i].status == false){
-        if(p[i].arrivalTime > time)
-            time = p[i].arrivalTime;
-        p[i].status = true;
-        time += p[i].burstTime;
-        p[i].completionTime = time;
-        p[i].turnaroundTime = p[i].completionTime - p[i].arrivalTime;
-        p[i].waitingTime = p[i].turnaroundTime - p[i].burstTime;
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].name, p[i].arrivalTime, p[i].burstTime, p[i].completionTime, p[i].turnaroundTime, p[i].waitingTime);
-        wait = wait + p[i].waitingTime;
-        turnaround = turnaround + p[i].turnaroundTime;
-        i = (i + 1) % n;
+    while (executedCount < n) {
+        int minIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (!p[i].executed && p[i].arrivalTime <= time) {
+                if (minIndex == -1 || p[i].burstTime < p[minIndex].burstTime) 
+                    minIndex = i;
+            }
+        }
+
+        if (minIndex != -1) {
+            if (p[minIndex].arrivalTime > time)
+                time = p[minIndex].arrivalTime;
+            p[minIndex].executed = true;
+            time += p[minIndex].burstTime;
+            p[minIndex].completionTime = time;
+            p[minIndex].turnaroundTime = p[minIndex].completionTime - p[minIndex].arrivalTime;
+            p[minIndex].waitingTime = p[minIndex].turnaroundTime - p[minIndex].burstTime;
+            printf("P%d\t%d\t%d\t%d\t%d\t%d\n", p[minIndex].name, p[minIndex].arrivalTime, p[minIndex].burstTime, p[minIndex].completionTime, p[minIndex].turnaroundTime, p[minIndex].waitingTime);
+            wait += p[minIndex].waitingTime;
+            turnaround += p[minIndex].turnaroundTime;
+            executedCount++;
+        }
+        else
+            time++;
     }
     printf("Average Waiting Time=%f\n",wait/n);
     printf("Average turnaround Time=%f\n",turnaround/n);
